c2932444ab0279db5bd45e8730d27dc4f9011e50
Mohammed Billoo <mab@mab-labs.com>
2021-02-17 17:48:30 -0500

    Add tracepoints demo

---
+++ ipc/demo/demo.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+#define TRACEPOINT_DEFINE
+#define TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#include "ust_tests_demo.h"
+#include "ust_tests_demo2.h"
+#include "ust_tests_demo3.h"
+
+int main(int argc, char **argv)
+{
+	int i, netint;
+	long values[] = { 1, 2, 3 };
+	char text[10] = "test";
+	double dbl = 2.0;
+	float flt = 2222.0;
+	int delay = 0;
+
+	if (argc == 2)
+		delay = atoi(argv[1]);
+
+	fprintf(stderr, "Demo program starting.\n");
+
+	sleep(delay);
+
+	fprintf(stderr, "Tracing... ");
+	tracepoint(ust_tests_demo, starting, 123);
+	for (i = 0; i < 5; i++) {
+		netint = htonl(i);
+		tracepoint(ust_tests_demo2, loop, i, netint, values,
+			   text, strlen(text), dbl, flt);
+	}
+	tracepoint(ust_tests_demo, done, 456);
+	tracepoint(ust_tests_demo3, done, 42);
+	fprintf(stderr, " done.\n");
+	return 0;
+}
+
---
+++ ipc/demo/tp2.c
@@ -0,0 +1,2 @@
+#define TRACEPOINT_CREATE_PROBES
+#include "ust_tests_demo2.h"
